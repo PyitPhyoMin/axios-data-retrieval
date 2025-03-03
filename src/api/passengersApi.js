9a6d175db50407fd66e0782b5c954206d97a89d9
PyitPhyoMin <pyitphyo.min@gmail.com>
2025-03-03 16:31:16 +0800

    made changes for service

--- src/api/passengersApi.js
+++ src/api/passengersApi.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 //Retrieve the information from ICA API
 //for the number of passengers cleared at Singapore Checkpoints.[PhyoMin]
-/* const datasetId = "d_e78db35440a41c0baff4e5f669532bd9";
+const datasetId = "d_e78db35440a41c0baff4e5f669532bd9";
 const BASE_URL =
-  "https://data.gov.sg/api/action/datastore_search?resource_id=" + datasetId; */
+  "https://data.gov.sg/api/action/datastore_search?resource_id=" + datasetId;
 
   // using DotNetCore API which is hosted on Render.com
-  const BASE_URL =
-  "https://webdotnetcoreapi-2.onrender.com/get-data";
-  const passengersApi = axios.create({ baseURL: BASE_URL });
+  /* const BASE_URL =
+  "https://webdotnetcoreapi-2.onrender.com/get-data";*/
+  const passengersApi = axios.create({ baseURL: BASE_URL }); 
 
 export default passengersApi;
